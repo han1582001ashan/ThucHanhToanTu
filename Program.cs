b8e9c8920e75fd6ffbef0ee8c53ed7c9a3ad57c9
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-05 21:27:15 +0700

    bt su dung toan tu

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Sudungtoantu
+{
+
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+
+            float area = width * height;
+            Console.WriteLine("S chu nhat: " + area);
+        }
+    }
+}
+
